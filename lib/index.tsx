5f04e82759ccd15e3d21de29dc69d2732c807fca
DaoZhang1588 <1205308740@qq.com>
2022-10-24 16:26:14 +0800

    添加icon的onClick事件

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,6 +2,9 @@ import Icon from './icon'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+const fn = () => {
+    console.log('fn')
+}
 ReactDOM.render(<div>
-    <Icon name='qq' />
+    <Icon name='qq' onClick={fn} />
 </div>, document.querySelector('#root'))
