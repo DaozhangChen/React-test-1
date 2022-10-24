c05f7400e24ee0c9c3a2d66a8c4a46726185f694
DaoZhang1588 <1205308740@qq.com>
2022-10-24 16:14:14 +0800

    添加scss-load

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import './importIcons';
+import './icon.scss'
 
 interface IconProps {
     name: string;
 }
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <span>
-            <svg>
-                <use xlinkHref={`#${props.name}`}></use>
-            </svg>
-        </span>
+        <svg className="fui-icon">
+            <use xlinkHref={`#${props.name}`}></use>
+        </svg>
     )
 }
 
