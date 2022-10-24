80068edf0b350bb54214a31e31209c07ab67f777
DaoZhang1588 <1205308740@qq.com>
2022-10-24 22:39:12 +0800

    修正Dialog的index

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,32 +1,41 @@
-import React, { Fragment } from "react";
+import React, { Fragment, MouseEventHandler, ReactElement } from "react";
 import './dialog.scss'
 import Icon from '../icon'
+import ReactDOM from "react-dom";
 
 interface Props {
     visible: boolean
     children: any | undefined
+    buttons: Array<ReactElement>
+    onClose?: MouseEventHandler
+    onCloseMask?: boolean
 }
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
-    return (
-        props.visible ?
-            <Fragment>
-                <div className="fui-dialog-mask"></div>
-                <div className="fui-dialog">
-                    <div className="fui-dialog-close">
-                        <Icon name='close' />
-                    </div>
-                    <header className="fui-dialog-header">提示</header>
-                    <main className="fui-dialog-main">{props.children}</main>
-                    <footer className="fui-dialog-footer">
-                        <button>ok</button>
-                        <button>cancel</button>
-                    </footer>
+    const x = props.visible ?
+        <Fragment>
+            <div className="fui-dialog-mask" onClick={props.onCloseMask ? props.onClose : undefined}></div>
+            <div className="fui-dialog">
+                <div className="fui-dialog-close" onClick={props.onClose}>
+                    <Icon name='close' />
                 </div>
-            </Fragment>
-            :
-            null
+                <header className="fui-dialog-header">提示</header>
+                <main className="fui-dialog-main">{props.children}</main>
+                <footer className="fui-dialog-footer">
+                    {
+                        props.buttons.map(
+                            (button, index) =>
+                                React.cloneElement(button, { key: index })
+                        )
+                    }
+                </footer>
+            </div>
+        </Fragment>
+        :
+        null;
 
+    return (
+        ReactDOM.createPortal(x, document.body)
     )
 }
 
--- lib/dialog/dialogExample.tsx
+++ lib/dialog/dialogExample.tsx
@@ -8,7 +8,12 @@ const DE = () => {
     return (
         <div>
             <button onClick={() => setX(!x)}>onclick</button>
-            <Dialog visible={x} >
+            <Dialog visible={x} buttons={
+                [
+                    <button onClick={() => setX(false)}>1</button>,
+                    <button onClick={() => setX(false)}>2</button>
+                ]
+            } onClose={() => setX(false)} onCloseMask={true}>
                 <strong>hi</strong>
             </Dialog>
         </div>
